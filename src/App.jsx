f31fa26a3da57fed031dab4dfeb9f5349c029f0d
roshansuthar1105 <roshansuthar2023@gmail.com>
2024-11-12 01:06:28 +0530

    resolving error

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import { useState } from 'react'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <div className='w-full bg-black text-white text-3xl'> Thirty six studio </div>
