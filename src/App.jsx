ebb96328f9ef96f28c025d137a869a35bbcc8de6
roshansuthar1105 <roshansuthar2023@gmail.com>
2024-11-16 19:41:51 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,20 @@ import { useState } from 'react'
 import Canvas from './components/Canvas'
 import data from './data.js'
 function App() {
-  // console.log("data is ",data);
   return (
     <>
-    <div className='w-full relative min-h-screen border-b-2 border-yellow-300 flex flex-row gap-3 flex-wrap p-4 justify-center align-middle bg-black text-white text-3xl'>
-    {data.map((item,index)=>{
-      return (
-        <div key={index} >
-          {item.map((canvasdetails ,index)=>{
+      <div className='w-screen overflow-hidden min-h-screen border-b-2 border-yellow-300'>
+        <div className='relative bg-slate-100 min-h-screen' >
+          {data[0].map((canvasdetails, index) => {
             return <Canvas details={canvasdetails} key={index} />
           })}
         </div>
-      )
-    })}
-    </div>
+        <div >
+          {data[1].map((canvasdetails, index) => {
+            return <Canvas details={canvasdetails} key={index} />
+          })}
+        </div>
+      </div>
     </>
   )
 }
--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -2,21 +2,21 @@ import React, { useEffect, useRef, useState } from 'react'
 import images from '../canvasimages.js'
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
-function Canvas({details}) {
-    const {numImages ,startIndex,duration,zIndex,left,top,size} =details;
+function Canvas({ details }) {
+    const { numImages, startIndex, duration, zIndex, left, top, size } = details;
     const canvasRef = useRef(null);
-    const [index , setIndex] =useState({value:startIndex});
-    useEffect(()=>{
-        console.log("from canvas ",details);
-    },[]);
-    useGSAP(()=>{
-        gsap.to(index,{
-            value:startIndex+numImages ,
-            duration:duration,
-            repeat:-1,
-            ease:"linear",
-            onUpdate:()=>{
-                setIndex({value:Math.round(index.value)})
+    const [index, setIndex] = useState({ value: startIndex });
+    useEffect(() => {
+        console.log("from canvas ", details);
+    }, []);
+    useGSAP(() => {
+        gsap.to(index, {
+            value: startIndex + numImages - 1,
+            duration: duration,
+            repeat: -1,
+            ease: "linear",
+            onUpdate: () => {
+                setIndex({ value: Math.round(index.value) })
             }
         })
     })
@@ -26,14 +26,17 @@ function Canvas({details}) {
         const img = new Image();
         img.src = images[index.value];
         img.onload = function () {
-            canvas.width=img.width;
-            canvas.height=img.height;
+            canvas.width = img.width;
+            canvas.height = img.height;
             ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
         };
     }, [index])
     return (
-        // fixed top-[${top}px] left-[${left}px] z-[${zIndex}] 
-        <canvas ref={canvasRef} style={{width:`${size}px`,height:`${size}px`,top:`${top}%`,left:`${left}%`,position:'fixed'}} id='canvas' ></canvas>
+        <>
+            {/* // fixed top-[${top}px] left-[${left}px] z-[${zIndex}]  */}
+            <canvas ref={canvasRef} style={{ width: `${size*1.4}px`, height: `${size*1.4}px`, top: `${top}%`, left: `${left}%`, position: 'absolute', zIndex: zIndex }} id='canvas' ></canvas>
+            {/* <canvas ref={canvasRef}  className={`absolute top-[${top}%] left-[${left}%] h-[${size}px] w-[${size}px] z-[${zIndex}]`}  id='canvas' ></canvas> */}
+        </>
     )
 }
 
